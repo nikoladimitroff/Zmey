212c35696a89bbd8aa354fce2bc1ddfadaa1bb07
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2017-05-06 15:46:29 +0300

    Fixed wrong indentation.

--- include/Zmey/LogHandler.h
+++ include/Zmey/LogHandler.h
@@ -2,19 +2,21 @@
 
 namespace Zmey
 {
-	enum class LogSeverity
-	{
-		Debug,
-		Trace,
-		Info,
-		Warning,
-		Error,
-		Fatal
-	};
-	class ILogHandler
-	{
-	public:
-		virtual ~ILogHandler() {}
-		virtual void WriteLog(LogSeverity severity, const char* message) = 0;
-	};
+
+enum class LogSeverity
+{
+	Debug,
+	Trace,
+	Info,
+	Warning,
+	Error,
+	Fatal
+};
+class ILogHandler
+{
+public:
+	virtual ~ILogHandler() {}
+	virtual void WriteLog(LogSeverity severity, const char* message) = 0;
+};
+
 }
--- include/Zmey/Memory/Allocator.h
+++ include/Zmey/Memory/Allocator.h
@@ -2,12 +2,14 @@
 
 namespace Zmey
 {
-	class IAllocator
-	{
-	public:
-		virtual ~IAllocator() {}
-		virtual void* Malloc(size_t size, unsigned alignment) = 0;
-		virtual void Free(void* ptr) = 0;
-		virtual void* Realloc(void* ptr, size_t newSize) = 0;
-	};
+
+class IAllocator
+{
+public:
+	virtual ~IAllocator() {}
+	virtual void* Malloc(size_t size, unsigned alignment) = 0;
+	virtual void Free(void* ptr) = 0;
+	virtual void* Realloc(void* ptr, size_t newSize) = 0;
+};
+
 }
