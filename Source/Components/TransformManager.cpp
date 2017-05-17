b750a0a1cb41a660d02553166127ab3bf741a0bf
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2017-05-17 22:39:51 +0300

    Implemented Json to binary conversion for the transform manager.

--- Source/Components/TransformManager.cpp
+++ Source/Components/TransformManager.cpp
@@ -12,23 +12,23 @@ namespace Components
 void TransformComponentToBlob(const nlohmann::json& rawJson, IDataBlob& blob)
 {
 	float position[3];
-	position[0] = rawJson["Position"][0];
-	position[1] = rawJson["Position"][1];
-	position[2] = rawJson["Position"][2];
-	blob.WriteData("Position", reinterpret_cast<uint8_t*>(position), sizeof(position));
+	position[0] = rawJson["position"][0];
+	position[1] = rawJson["position"][1];
+	position[2] = rawJson["position"][2];
+	blob.WriteData("position", reinterpret_cast<uint8_t*>(position), sizeof(position));
 
 	float rotation[4];
-	rotation[0] = rawJson["Rotation"][0];
-	rotation[1] = rawJson["Rotation"][1];
-	rotation[2] = rawJson["Rotation"][2];
-	rotation[2] = rawJson["Rotation"][3];
-	blob.WriteData("Rotation", reinterpret_cast<uint8_t*>(rotation), sizeof(rotation));
+	rotation[0] = rawJson["rotation"][0];
+	rotation[1] = rawJson["rotation"][1];
+	rotation[2] = rawJson["rotation"][2];
+	rotation[2] = rawJson["rotation"][3];
+	blob.WriteData("rotation", reinterpret_cast<uint8_t*>(rotation), sizeof(rotation));
 
 	float scale[3];
-	scale[0] = rawJson["Scale"][0];
-	scale[1] = rawJson["Scale"][1];
-	scale[2] = rawJson["Scale"][2];
-	blob.WriteData("Scale", reinterpret_cast<uint8_t*>(scale), sizeof(scale));
+	scale[0] = rawJson["scale"][0];
+	scale[1] = rawJson["scale"][1];
+	scale[2] = rawJson["scale"][2];
+	blob.WriteData("scale", reinterpret_cast<uint8_t*>(scale), sizeof(scale));
 }
 
 }
