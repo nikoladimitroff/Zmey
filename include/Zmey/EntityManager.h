57257b59911e3faa8c3dbf55b026403f533b4b50
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2017-05-20 12:42:06 +0300

    Added operator== and hash for EntityId.

--- include/Zmey/EntityManager.h
+++ include/Zmey/EntityManager.h
@@ -9,6 +9,11 @@ struct EntityId
 	EntityId(const EntityId&) = default;
 	EntityId& operator=(const EntityId&) = default;
 	using IndexType = uint32_t;
+
+	friend bool operator==(const EntityId& lhs, const EntityId& rhs)
+	{
+		return std::memcmp(&lhs, &rhs, sizeof(EntityId)) == 0;
+	}
 private:
 	EntityId(uint32_t index, uint16_t generation)
 		: Index(index)
@@ -32,6 +37,19 @@ public:
 private:
 	stl::vector<uint16_t> m_Generation;
 	stl::queue<uint_fast32_t> m_FreeIndices;
+};
+
+}
+
+namespace std
+{
+template<>
+struct hash<Zmey::EntityId>
+{
+inline std::size_t operator()(const Zmey::EntityId& id) const
+{
+	return *reinterpret_cast<const size_t*>(&id);
+}
 };
 
 }
