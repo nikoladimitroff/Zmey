8328802d602ccae967992e377f0142a84f8a903b
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2017-05-24 01:06:31 +0300

    The idl compiler now generated method code.

--- Tools/IdlCompiler/idl_compiler.js
+++ Tools/IdlCompiler/idl_compiler.js
@@ -1,3 +1,4 @@
+const assert = require("assert");
 const fs = require("fs");
 const path = require("path");
 const os = require("os");
@@ -5,7 +6,7 @@ const os = require("os");
 const RegexLibrary = {
     // Matches everything between the interface keyword and the closing }
     // Group 1 is the name of the interface
-    Interface: /interface\s+(\w+)\s\{[\s\S]*?\}/g,
+    Interface: /\s*interface\s+(\w+)\s\{[\s\S]*?\}\s*/g,
     Attribute: /attribute\s+(\w+)\s+(\w+);/g,
     // The following akward expression is repeated inside both method and ctor:
     // \(((:?(:?\s*\w+\s+\w+,)*(:?\s*\w+\s+\w+))?)\)
@@ -17,7 +18,15 @@ const RegexLibrary = {
 
 class ChakraGlueGenerator {
     generateDefinitionForArg(arg) {
-        return `${arg.type} _${arg.name};`;
+        switch (arg.type) {
+            case "string":
+                return `stl::string _${arg.name};`;
+            case "float":
+            case "double":
+            case "int":
+            default:
+                return `${arg.type} _${arg.name};`;
+        }
     }
     generateParserForArg(arg) {
         let code = null;
@@ -29,10 +38,24 @@ class ChakraGlueGenerator {
             case "int":
                 code = `JSNumberToInt(arguments[${arg.index}], &_${arg.name});`;
                 return code;
+            case "string":
+                code = `
+	JsValueRef _${arg.name}stringValue;
+	JsConvertValueToString(arguments[${arg.index}], &_${arg.name}stringValue);
+	const wchar_t* _${arg.name}string;
+	size_t _${arg.name}Stringlength;
+	JsStringToPointer(_${arg.name}stringValue, _${arg.name}string, &_${arg.name}Stringlength);
+	stl::string _${arg.name} = WStringToUtf8(_${arg.name}String, _${arg.name}Stringlength);
+`;
+                return code;
+            case "long":
+            default:
+                assert(false, "Unsupported type: " + arg.type);
         }
     }
     generateParserForArgList(argList) {
         return argList.map(arg => {
+            console.log(arg);
             const code =`
 	${this.generateDefinitionForArg(arg)}
 	${this.generateParserForArg(arg)}
@@ -66,24 +89,30 @@ JsValueRef CALLBACK JS${interfaceName}Constructor(JsValueRef callee, bool isCons
     }
 
     generateMethod(interfaceName, methodName, argList) {
-        const argParsingCode = "";
-        const argCode = "";
+        const argParsingCode = this.generateParserForArgList(argList);
+        const argListCode = this.generateListForArgList(argList);
         const cppCode =
 `
-JsValueRef CALLBACK JS${interfaceName}Method(JsValueRef callee, bool isConstructCall, JsValueRef *arguments, unsigned short argumentCount, void *callbackState)
+JsValueRef CALLBACK JS${interfaceName}${methodName}(JsValueRef callee, bool isConstructCall, JsValueRef *arguments, unsigned short argumentCount, void *callbackState)
 {
 	assert(!isConstructCall && argumentCount == ${argList.length});
 	JsValueRef output = JS_INVALID_REFERENCE;
 	void* object;
-	if (JsGetExternalData(arguments[0], &object) == JsNoError) {
-		${interfaceName}* ptr = static_cast<${interfaceName}*>(object);
-		${argParsingCode}
-		ptr->${methodName}(${argCode});
-	};
+	if (JsGetExternalData(arguments[0], &object) != JsNoError) {
+		return output;
+	}
+	${interfaceName}* ptr = static_cast<${interfaceName}*>(object);
+	${argParsingCode}
+	ptr->${methodName}(${argListCode});
+
 	return output;
 }
 `
         return cppCode;
+    }
+
+    generateProperty(interfaceName, type, name) {
+        return "";
     }
 
 }
@@ -104,16 +133,18 @@ class IdlCompiler {
         }
     }
     compileSingleFile(error, fileContents) {
-        let interfaceMatch = RegexLibrary.Interface.exec(fileContents);
-        if (!interfaceMatch) {
-            console.error("Couldn't find an interface in file!");
-            return;
+        while (RegexLibrary.Interface.lastIndex != fileContents.length) {
+            let interfaceMatch = RegexLibrary.Interface.exec(fileContents);
+            if (!interfaceMatch) {
+                console.error("Couldn't find an interface in file!");
+                return;
+            }
+            const interfaceName = interfaceMatch[1];
+            const interfaceBodyStart = interfaceMatch[0].indexOf("{") + 1;
+            const interfaceBodyEnd = interfaceMatch[0].indexOf("}");
+            const interfaceBody = interfaceMatch[0].substring(interfaceBodyStart, interfaceBodyEnd);
+            this.compileSingleInterface(interfaceName, interfaceBody);
         }
-        const interfaceName = interfaceMatch[1];
-        const interfaceBodyStart = interfaceMatch[0].indexOf("{") + 1;
-        const interfaceBodyEnd = interfaceMatch[0].indexOf("}");
-        const interfaceBody = interfaceMatch[0].substring(interfaceBodyStart, interfaceBodyEnd);
-        this.compileSingleInterface(interfaceName, interfaceBody);
     }
     compileSingleInterface(interfaceName, interfaceBody) {
         let finalGlueCode = "";
@@ -127,24 +158,35 @@ class IdlCompiler {
     }
     parseFirstStatement(interfaceName, body) {
         let match = null;
-        let glueCode = null;
+        let glueCode = "";
         let lastIndex = body.length;
         if (match = RegexLibrary.Constructor.exec(body)) {
             const args = this.parseArgs(match[1]);
             glueCode = this.generator.generateConstructor(interfaceName, args);
             lastIndex = RegexLibrary.Constructor.lastIndex;
-        }
-        if (match = RegexLibrary.Attribute.exec(body)) {
-
+            RegexLibrary.Constructor.lastIndex = 0;
         }
         if (match = RegexLibrary.Method.exec(body)) {
-
+            /* const returnType = match[1]; unused*/
+            const method = match[2];
+            const args = this.parseArgs(match[3]);
+            glueCode = this.generator.generateMethod(interfaceName, method, args);
+            lastIndex = RegexLibrary.Constructor.lastIndex;
+            RegexLibrary.Constructor.lastIndex = 0;
+        }
+        if (match = RegexLibrary.Attribute.exec(body)) {
+            const type = match[1];
+            const name = match[2];
+            glueCode = this.generator.generateProperty(interfaceName, type, name);
+            lastIndex = RegexLibrary.Constructor.lastIndex;
+            RegexLibrary.Constructor.lastIndex = 0;
         }
         return [glueCode, lastIndex];
     }
     parseArgs(argsAsString) {
         return argsAsString.split(/,/g)
             .map(s => s.trim())
+            .filter(s => s.length)
             .map((arg, i) => ({ type: arg.split(" ")[0], name: arg.split(" ")[1], index: i }));
     }
 }
