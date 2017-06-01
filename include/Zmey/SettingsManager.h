4944f113c311f346aa8d32337d1f5d41d5d798ab
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2017-06-02 00:39:21 +0300

    Added forgotten settings manager header.

---
+++ include/Zmey/SettingsManager.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <stdint.h>
+
+#include <Zmey/Memory/MemoryManagement.h>
+
+namespace Zmey
+{
+struct SettingsInternalParserHandle;
+class SettingsHandle
+{
+public:
+	SettingsHandle(SettingsInternalParserHandle& internalHandle, const stl::string& section, class SettingsManager& owner);
+	~SettingsHandle();
+
+	tmp::string ReadValue(const stl::string& key, const stl::string& defaultValue = "");
+	void WriteValue(const stl::string& key, const stl::string& value);
+
+	bool ReadValue(const stl::string& key, bool defaultValue = false);
+	void WriteValue(const stl::string& key, bool value);
+
+	int32_t ReadValue(const stl::string& key, int32_t defaultValue = 0);
+	void WriteValue(const stl::string& key, int32_t value);
+
+	float ReadValue(const stl::string& key, float defaultValue = 0.f);
+	void WriteValue(const stl::string& key, float value);
+
+	void DeleteValue(const stl::string& key);
+private:
+	const stl::string& m_Section;
+	SettingsInternalParserHandle& m_Settings;
+	class SettingsManager& m_Owner;
+};
+
+class SettingsManager
+{
+public:
+	SettingsManager();
+	stl::shared_ptr<SettingsHandle> DataFor(const stl::string& settingName);
+private:
+	void OnSettingsClosed(const stl::string& settingsName);
+	stl::string m_FilePath;
+	std::unique_ptr<SettingsInternalParserHandle> m_Serializer;
+	stl::unordered_map<stl::string, stl::shared_ptr<SettingsHandle>> m_OpenSettingsMap;
+	friend class SettingsHandle;
+};
+
+}
