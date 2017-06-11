b075d08b3e70969913cd4a7c1c1b511c817668f7
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2017-06-11 14:47:50 +0300

    Added examples of Tag usage.

--- Games/GiftOfTheSanctum/Content/Scripts/main.js
+++ Games/GiftOfTheSanctum/Content/Scripts/main.js
@@ -3,9 +3,12 @@ let actor = {
     interval: console.error.bind(console, "kopr")
 };
 
+let tagManager = world.getManager(Manager.Tag);
+const entity = tagManager.findFirstByTag(new Name("Player1"));
+
 let nextFrame = function (delta) {
     let transformManager = world.getManager(Manager.Transform);
-    let transform = transformManager.lookup(world.getFirstEntity());
+    let transform = transformManager.lookup(entity);
     let posy = new Vector3(0.1, 0, 0);
     //posy.x = 0.2;
     let pozz = transform.position();
