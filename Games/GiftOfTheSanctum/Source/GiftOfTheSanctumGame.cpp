36249b3a564978a96199af80d40556d1bf87e3f7
NDobrev <martindobrev0@gmail.com>
2018-01-04 00:26:55 +0200

    Implement Spell expire Add prefix m_ to all members in SpellComponentManager

--- Games/GiftOfTheSanctum/Source/GiftOfTheSanctumGame.cpp
+++ Games/GiftOfTheSanctum/Source/GiftOfTheSanctumGame.cpp
@@ -94,6 +94,10 @@ void GiftOfTheSanctumGame::Initialize()
 
 	// SetupPlayersToSpawnPoints
 	SetupPlayersToSpawnPoints();
+
+	GetWorld()->GetManager<SpellComponent>().SetSpellExpireListener([this](Zmey::EntityId id) {
+		GetWorld()->DestroyEntity(id);
+	});
 }
 
 void GiftOfTheSanctumGame::SetupPlayersToSpawnPoints()
@@ -120,13 +124,13 @@ void GiftOfTheSanctumGame::CastSpell(uint8_t playerIndex, uint8_t spellIndex)
 
 
 	//Append spell instance to active spells
-	auto spellManager = GetWorld()->GetManager<SpellComponent>();
+	auto& spellManager = GetWorld()->GetManager<SpellComponent>();
 	spellManager.Push(SpellComponent::EntryDescriptor{
 	1.0f,//InitialSpeed
 	1.0f,//ImpactDamage
 	1.0f,//InitialMass
 	1.0f,//CooldownTime
-	1.0f,//ExpireTime
+	1.0f,//LifeTime
 	spellId//EntityId
 		});
 }
--- Games/GiftOfTheSanctum/Source/SpellComponentManager.cpp
+++ Games/GiftOfTheSanctum/Source/SpellComponentManager.cpp
@@ -6,9 +6,39 @@ void SpellComponent::InitializeFromBlob(const Zmey::tmp::vector<Zmey::EntityId>&
 
 }
 
+template<typename T>
+void NotSaveErase(T& array, size_t index)
+{
+	array[index] = array[array.size() - 1];
+	array.resize(array.size() - 1);
+}
+
 void SpellComponent::Simulate(float deltaTime)
 {
+	Zmey::tmp::vector<size_t> forErase;
+	for(size_t i = 0; i < m_LifeTime.size(); i++)
+	{
+		m_LifeTime[i] -= deltaTime;
+		if (m_LifeTime[i] < 0.f)
+		{
+			forErase.push_back(i);
+		}
+	}
 
+	for (auto it = forErase.rbegin(); it != forErase.rend(); ++it)
+	{
+		if (m_SpellExpireCallback)
+		{
+			m_SpellExpireCallback(m_EntityId[*it]);
+		}
+		
+		NotSaveErase(m_InitialSpeed, *it);
+		NotSaveErase(m_ImpactDamage, *it);
+		NotSaveErase(m_InitialMass, *it);
+		NotSaveErase(m_CooldownTime, *it);
+		NotSaveErase(m_LifeTime, *it);
+		NotSaveErase(m_EntityId, *it);
+	}
 }
 
 void SpellComponent::RemoveEntity(Zmey::EntityId id)
@@ -18,12 +48,12 @@ void SpellComponent::RemoveEntity(Zmey::EntityId id)
 
 void SpellComponent::Push(SpellComponent::EntryDescriptor desc)
 {
-	InitialSpeed.push_back(desc.InitialSpeed);
-	ImpactDamage.push_back(desc.ImpactDamage);
-	InitialMass.push_back(desc.InitialMass);
-	CooldownTime.push_back(desc.CooldownTime);
-	ExpireTime.push_back(desc.ExpireTime);
-	EntityId.push_back(desc.EntityId);
+	m_InitialSpeed.push_back(desc.InitialSpeed);
+	m_ImpactDamage.push_back(desc.ImpactDamage);
+	m_InitialMass.push_back(desc.InitialMass);
+	m_CooldownTime.push_back(desc.CooldownTime);
+	m_LifeTime.push_back(desc.LifeTime);
+	m_EntityId.push_back(desc.EntityId);
 }
 
 
