cc01f8dd6da325164a926767847b0f51f533e301
martin dobrev <martindobrev0@gmail.com>
2018-08-29 21:19:28 +0300

    Enchance biome generation.

--- Prototypes/Overmind/src/terrain.ts
+++ Prototypes/Overmind/src/terrain.ts
@@ -1,6 +1,5 @@
 
-noise.seed(12);
-
+noise.seed(129);
 // each terrain has 3x6 tiles
 // each tile is 32/32 pixels
 
@@ -22,6 +21,10 @@ noise.seed(12);
 // blic bi bric
 
 
+function isInRange(range: any, v: number): boolean {
+    return range.max >= v && range.min <= v;
+}
+
 export class Terrain {
 
     constructor() {
@@ -33,6 +36,17 @@ export class Terrain {
                                               32, 32, target.x , target.y , Terrain.tileSize, Terrain.tileSize);
     }
 
+    private static findBiome(tileDescr: any): any {
+        for(let i = 0; i < Terrain.biomes.length; ++i) {
+            const current = Terrain.biomes[i];
+            if (isInRange(current.latitude, tileDescr.latitude) &&
+            isInRange(current.humidity, tileDescr.humidity) && true
+            /*isInRange(current.temperature, tileDescr.temperature)*/) {
+                return current;
+            }
+        }
+        return null;
+    }
     public render(context: CanvasRenderingContext2D, tiles: HTMLImageElement): void {
         let sizeW = Math.floor(context.canvas.width / Terrain.tileSize);
         let sizeH = Math.floor(context.canvas.height / Terrain.tileSize);
@@ -40,26 +54,15 @@ export class Terrain {
         for (let i = 0; i < sizeH; ++i) {
             for (let j = 0; j < sizeW; ++j) {
                 const ele = noise.simplex2(j / 50, i / 50);
-
+                const humi = (noise.simplex2(1000+ (j / 50), 1000 + (i / 50)) + 1) / 2;
                 const target = {x: j * Terrain.tileSize,
                                 y: i * Terrain.tileSize};
-                if (ele > 0.8) {
-                    Terrain.drawTile(context, tiles, "Snow", "mid", target);
-                }
-                else if(ele > 0.6) {
-                    Terrain.drawTile(context, tiles, "BlackDirt", "mid", target);
-                }
-                else if(ele > 0.5) {
-                    Terrain.drawTile(context, tiles, "LightDirt", "mid", target);
-                }
-                else if(ele > 0.0) {
-                    Terrain.drawTile(context, tiles, "Grass", "mid", target);
-                }
-                else if (ele > -0.3) {
-                    Terrain.drawTile(context, tiles, "Sand", "mid", target);
+                const biome = Terrain.findBiome({latitude: ele, humidity: humi, temperature: 0});
+                if (biome) {
+                    Terrain.drawTile(context, tiles, biome.tileName, "mid", target);
                 }
                 else {
-                    Terrain.drawTile(context, tiles, "Water", "mid", target);
+                    console.error(ele + " " + humi);
                 }
             }
         }
@@ -67,6 +70,93 @@ export class Terrain {
      }
 
     private static tileSize = 32;
+    private static biomes = [
+
+        {
+        name: 'sea',
+        latitude: {min: -1.0, max: -0.3},
+        humidity: {min: 0, max: 1.0},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "Water",
+    },
+        {
+        name: 'beach',
+        latitude: {min: -0.3, max: 0.1},
+        humidity: {min: 0.0, max: 0.3},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "Sand",
+    },
+            {
+        name: 'beach',
+        latitude: {min: -0.3, max: -0.2},
+        humidity: {min: 0.0, max: 1.0},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "Sand",
+    },
+            {
+        name: 'grassland',
+        latitude: {min: -0.3, max: 0.7},
+        humidity: {min: 0.3, max: 0.6},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "Grass",
+    },
+            {
+        name: 'forest',
+        latitude: {min: -0.3, max: 0.7},
+        humidity: {min: 0.6, max: 0.9},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "DarkGrass",
+    },
+            {
+        name: 'tropicalRainForest',
+        latitude: {min: -0.3, max: 0.7},
+        humidity: {min: 0.9, max: 1.0},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "LongGrass",
+    },
+                {
+        name: 'subtropicalDesert',
+        latitude: {min: 0.1, max: 0.4},
+        humidity: {min: 0.0, max: 0.3},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "Sand",
+    },
+                {
+        name: 'temperedDesert',
+        latitude: {min: 0.4, max: 0.8},
+        humidity: {min: 0.0, max: 0.3},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "BlackDirt",
+    },
+                {
+        name: 'scorched',
+        latitude: {min: 0.8, max: 1.0},
+        humidity: {min: 0.0, max: 0.3},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "GreyDirt",
+    },
+                {
+        name: 'bare',
+        latitude: {min: 0.7, max: 1.0},
+        humidity: {min: 0.3, max: 0.5},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "GreyDirt",
+    },
+                {
+        name: 'tundra',
+        latitude: {min: 0.7, max: 1.0},
+        humidity: {min: 0.5, max: 0.8},
+        temperature: {min: 0.0, max: 1.0},
+        tileName: "BlackDirt",
+    },
+        {
+        name: 'icesheet',
+        latitude: {min: 0.7, max: 1.0},
+        humidity: {min: 0.8, max: 1.0},
+        temperature: {min: -1.0, max: 1.0},
+        tileName: "Snow",
+    },
+    ];
     private static tilePositions: any = { misk1: {x : 0, y: 0  }, tloc: {x : 32, y: 0  }, troc: {x : 64, y: 0  },
                                           misc2: {x : 0, y: 32 }, bloc: {x : 32, y: 32 }, broc: {x : 64, y: 32 },
                                           tlic:  {x : 0, y: 64 }, ti  : {x : 32, y: 64 }, tric: {x : 64, y: 64 },
